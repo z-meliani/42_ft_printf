a6be2657ac2b247e828b847c2ad76a895e861dcb
Z_Meliani <78383351+z-meliani@users.noreply.github.com>
2024-11-26 11:33:10 +0100

    Update test_ft_printf.c

--- test_ft_printf.c
+++ test_ft_printf.c
@@ -12,6 +12,8 @@
 
 #include "ft_printf.h"
 
+#include "ft_printf.h"
+
 int	main(void)
 {
 	char	*str = "c -> %c\ns -> %s\np -> %p\nd -> %d\ni -> %i\nu -> %u\nx -> %x\nX -> %X\n% -> %%\n";
@@ -19,7 +21,7 @@ int	main(void)
 	char	c = 'W';
 	long	pt = 9876543210;
 	void	*pt0 = &pt;
-	int		len;
+	int	len;
 
 	printf("-------- printf / ft_printf--------\n");
 	len = printf(str, c, sent, pt, pt, pt, pt, pt, pt);
@@ -30,6 +32,11 @@ int	main(void)
 	len = printf(str, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 	printf("len --> %d\n\n", len);
 	len = ft_printf(str, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	ft_printf("len --> %d\n\n", len);
+	printf("-------- printf / ft_printf (EMPTY) --------\n");
+	len = printf(0);
+	printf("len --> %d\n\n", len);
+	len = ft_printf(0);
 	ft_printf("len --> %d\n\n", len);
 	ft_printf("------- mix test -------\n");
 	printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%%%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42,'C', "0", 0, 0 ,0 ,0, 42, 0);
